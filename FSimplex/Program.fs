d88c18bf0ea80fb824fd30e0503f963a2d3d9f73
Holger Borum <blaa.vogn@gmail.com>
2016-05-30 22:50:01 +0200

    Transformed to tableau

--- FSimplex/Program.fs
+++ FSimplex/Program.fs
@@ -1,20 +1,22 @@
 ﻿open Fraction
 
-type rest = int * int list
+type Rest = int * int list
 
 let printDic dic = 
-  Array.iter 
-    (fun a -> Array.iter (fun e -> printf "%A" e) a; printf "\n")
+  List.iter 
+    (fun a -> printf "%d:" (fst a); List.iter (fun e -> printf "%A" e) (snd a); printf "\n")
     dic
 
-let toTableau (d: 'a list) =
+let toTableau (d: Frac list list) =
   let slackVars = d.Length
   let augZ = List.init (slackVars-1) (fun _ -> 0)
   let augs = List.map (
-                         fun l -> List.map (fun e -> if e = l then 1 else 0) augZ
+                         fun l -> List.mapi (fun i _ -> if i = l then Frac(1,1) else Frac(0,1)) augZ
                        ) [0..slackVars-1]
-  let splice l a = List.fold (fun (i,ac) e -> if i = slackVars - 2 then e::aug@ac) [] l
-  List.map2 (fun l a -> splice l a) 
+
+  let splice l aug =
+    snd <| List.fold (fun (i,ac) e -> if i = slackVars - 1 then (i+1,ac@aug@[e]) else (i+1,ac@[e])) (0,[]) l
+  List.mapi2 (fun i l a -> (i + slackVars, splice l a)) d augs 
 
 [<EntryPoint>]
 let main argv = 
@@ -25,6 +27,8 @@ let main argv =
        [Frac(1,1);Frac(0,1);Frac(0,1)]
      ]
   
-  printDic input
+ // printDic input
+  printfn ""
+  printDic <| toTableau input
   
   0 // return an integer exit code
