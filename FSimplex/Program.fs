0b661db769f7b3b4efd9e9895522b7668f23d33c
Holger Borum <blaa.vogn@gmail.com>
2016-05-30 22:28:31 +0200

    VS crashed

--- FSimplex/Program.fs
+++ FSimplex/Program.fs
@@ -1,12 +1,30 @@
 ﻿open Fraction
 
+type rest = int * int list
+
+let printDic dic = 
+  Array.iter 
+    (fun a -> Array.iter (fun e -> printf "%A" e) a; printf "\n")
+    dic
+
+let toTableau (d: 'a list) =
+  let slackVars = d.Length
+  let augZ = List.init (slackVars-1) (fun _ -> 0)
+  let augs = List.map (
+                         fun l -> List.map (fun e -> if e = l then 1 else 0) augZ
+                       ) [0..slackVars-1]
+  let splice l a = List.fold (fun (i,ac) e -> if i = slackVars - 2 then e::aug@ac) [] l
+  List.map2 (fun l a -> splice l a) 
 
 [<EntryPoint>]
 let main argv = 
-  let f1 = Frac(3,7)
-  let f2 = Frac(4,7)
-    
-  let res = f1 + f2
-
-  printfn "%s" <| res.ToString ()
+  let input =
+     [
+       [Frac(1,1);Frac(2,1);Frac(1,1)];
+       [Frac(2,1);Frac(2,1);Frac(3,1)];
+       [Frac(1,1);Frac(0,1);Frac(0,1)]
+     ]
+  
+  printDic input
+  
   0 // return an integer exit code
