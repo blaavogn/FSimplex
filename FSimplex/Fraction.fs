2a0313d5549ec7fefe847f9ca3ad3d7b0e6273a8
Holger Borum <blaa.vogn@gmail.com>
2016-05-30 21:48:42 +0200

    Fractions

---
+++ FSimplex/Fraction.fs
@@ -0,0 +1,17 @@
+﻿module Fraction
+
+let rec GCD (a:int) (b: int) = 
+    if b = 0 then a else GCD b a%b
+
+
+type Frac(n: int,d: int) = 
+  member x.n = n 
+  member x.d = d 
+
+
+   
+  
+  static member reduce (f1: Frac) =
+   
+
+  static member inline (+!) (f1: Frac,f2: Frac) = Frac(f1.n+f2.n, f1.d+f2.d)
